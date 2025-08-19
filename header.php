6c185433a9ab909c686aab545823565324077ee6
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 17:40:45 +0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="logo">CNN Clone</div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="category.php?cat=World">World</a></li>
+            <li><a href="category.php?cat=Sports">Sports</a></li>
+            <li><a href="category.php?cat=Technology">Technology</a></li>
+            <li><a href="category.php?cat=Entertainment">Entertainment</a></li>
+        </ul>
+    </nav>
+</header>
